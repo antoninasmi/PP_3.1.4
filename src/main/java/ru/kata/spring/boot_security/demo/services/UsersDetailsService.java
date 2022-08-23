9544bf90cd0a43ee301619bee379f348c6abaa75
antoninasmi <s.antonina.g@gmail.com>
2022-08-23 14:39:35 +0300

    Modified PP_3.1.4
    Modified PP_3.1.4

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UsersDetailsService.java
@@ -0,0 +1,30 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.models.User;
+
+@Service
+public class UsersDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found!", username));
+        }
+        return user;
+    }
+}
